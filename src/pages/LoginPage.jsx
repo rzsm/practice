fbead7335460b4d907919fd3ac282548d0dac6b2
Raziye <rosemiller.developer@gmail.com>
2022-08-04 10:40:35 +0430

    Add: useToggle custom hook

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,29 +1,39 @@
-import { useState } from "react";
+import useToggle from "../hooks/useToggle";
 import Card from "../components/UI/Card";
 import Input from "../components/UI/Input";
 import Button from "../components/UI/Button";
 import styles from './LoginPage.module.css'
 
 const LoginPage = () => {
-    const [isLoginForm, setIsLoginForm] = useState(true);
-
-    const pageTitle = isLoginForm ? "Login" : "Sign Up";
-    const submitBtnTxt = isLoginForm ? "Login" : "Create Account";
-    const buttonBtnTxt = isLoginForm ? "Create new account" : "Login with existing account";
-
-    const changeLoginHandler = () => {
-        setIsLoginForm((prev) => !prev);
-    }
-
+    const [isLogin, toggleLogin] = useToggle(true); 
+    
     return (
         <Card className={styles.card}>
-            <h1 className={styles['card-title']}> {pageTitle} </h1>
+            <h1 className={styles['card-title']}> 
+                {isLogin ? "Login" : "Sign Up"} 
+            </h1>
             <form className={styles['login-form']}>
-                <Input label="Your Email" id="email" required inputClass={styles.input}/>
-                <Input label="Your Password" id="password" required inputClass={styles.input + ' ' + styles['last-input']}/>
-                <Button className={styles['login-btn']}> {submitBtnTxt} </Button>
-                <button className={styles['new-account']} type="button" onClick={changeLoginHandler}>
-                    {buttonBtnTxt}
+                <Input 
+                    label="Your Email"
+                    id="email" 
+                    required 
+                    inputClass={styles.input}
+                />
+                <Input 
+                    label="Your Password" 
+                    id="password" 
+                    required 
+                    inputClass={styles.input + ' ' + styles['last-input']}
+                />
+                <Button className={styles['login-btn']}> 
+                    {isLogin ? "Login" : "Create Account"} 
+                </Button>
+                <button 
+                    className={styles['new-account']} 
+                    type="button" 
+                    onClick={toggleLogin}
+                >
+                    {isLogin ? "Create new account" : "Login with existing account"}
                 </button>
             </form>
         </Card>
