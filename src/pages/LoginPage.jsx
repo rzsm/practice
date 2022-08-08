bf03f7a0098b70bbd6952067986ff34a250d8d80
Raziye <rosemiller.developer@gmail.com>
2022-08-08 22:48:00 +0430

    Add: Feedback user about sign-up process

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useToggle from "../hooks/useToggle";
 import useFetch from "../hooks/useFetch";
 import Card from "../components/UI/Card";
@@ -9,9 +9,11 @@ import styles from './LoginPage.module.css'
 const LoginPage = () => {
     const [isLogin, toggleLogin] = useToggle(true); 
     const [email, setEmail] = useState();
-    const [password, setPassword] = useState();     
+    const [password, setPassword] = useState();   
     const API_KEY = 'AIzaSyDkgsq8Ui7-n5sOgpLFcdxNoVEhAKmtEtE';
-    const {loading, error, value, asyncFn: signUp} = useFetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+    // Create Sign-up related states and function using useFetch hook
+    const {loading: signUpLoading, error: signUpError, value: SignUpResponse, asyncFn: signUp} 
+    = useFetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
     {
         method:"POST",
         body : JSON.stringify({
@@ -21,9 +23,22 @@ const LoginPage = () => {
         })
     })
     
+    // Loading variaable based on loading states returned by useFetch for UX goal
+    let loading = signUpLoading;
+
+    // Feedback user what was wrong
+    let errMsg = "";
+    useEffect(() => {
+        if (signUpError) {
+            errMsg = signUpError.error.message ? signUpError.error.message : "Authentication Failed";
+            alert(errMsg);
+        }
+    }, [signUpError])
+
+    // Handle Sign Up and Login
     const submitHandler = (event) => {
         event.preventDefault();
-        if (!isLogin) signUp();                
+        if (!isLogin) signUp();              
     }
     
     return (
@@ -31,7 +46,7 @@ const LoginPage = () => {
             <h1 className={styles['card-title']}> 
                 {isLogin ? "Login" : "Sign Up"} 
             </h1>
-            <form className={styles['login-form']}>
+            <form className={styles['login-form']} onSubmit={submitHandler}>
                 <Input 
                     label="Your Email"
                     id="email" 
@@ -47,16 +62,21 @@ const LoginPage = () => {
                     inputClass={styles.input + ' ' + styles['last-input']}
                     onChange={e => setPassword(e.target.value)}                    
                 />
-                <Button className={styles['login-btn']} onClick={submitHandler}> 
-                    {isLogin ? "Login" : "Create Account"} 
-                </Button>
+                {!loading &&
+                    <Button className={styles['login-btn']}> 
+                        {isLogin ? "Login" : "Create Account"} 
+                    </Button>
+                }
+                {loading && <p className={styles.loading}>Sending Request ... </p>}              
                 <button 
                     className={styles['new-account']} 
                     type="button" 
                     onClick={toggleLogin}
-                >
+                >                    
                     {isLogin ? "Create new account" : "Login with existing account"}
                 </button>
+            
+
             </form>
         </Card>
     )
