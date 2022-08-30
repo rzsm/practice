6f73179702844c99e08fe83643356ed07f7b2291
Raziye <rosemiller.developer@gmail.com>
2022-08-30 13:08:50 +0430

    modal animation by CSS transitions

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react"
+import "./App.css"
+import Modal from "./components/Modal/Modal"
+import Backdrop from "./components/Backdrop/Backdrop"
+import List from "./components/List/List"
+
+const App = () => {
+	const [isModalOpen, setIsModalOpen] = useState(false)
+
+	const showModal = () => {
+		setIsModalOpen(true)
+	}
+
+	const closeModal = () => {
+		setIsModalOpen(false)
+	}
+
+	return (
+		<div className="App">
+			<h1>React Animations</h1>
+			<Modal show={isModalOpen} close={closeModal} />
+			<Backdrop show={isModalOpen} />
+			<button className="Button" onClick={showModal}>
+				Open Modal
+			</button>
+			<h3>Animating Lists</h3>
+			<List />
+		</div>
+	)
+}
+
+export default App
