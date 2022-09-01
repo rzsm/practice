92d0e3c2631fd48279097d74fb06ac3073211c69
Raziye <rosemiller.developer@gmail.com>
2022-09-01 10:46:35 +0430

    Cleaner code with CSSTransition

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,39 +1,28 @@
-import React from 'react';
-import { Transition } from 'react-transition-group';
-import './Modal.css';
+import React from "react"
+import { CSSTransition } from "react-transition-group"
+import "./Modal.css"
 
-const modal = (props) => {    
-
-    return (
-		// we can have animation whenever modal is closed and also unmount it from DOM
-		// which is something can not be achieved by only CSS animatoin
-			<Transition 
-				in={props.show}
-				timeout={300}
-				mountOnEnter
-				unmountOnExit
-			>
-				{state => {					
-					const cssClasses = [
-						"Modal",
-						 state === "entering" 
-						 ? "ModalOpen" 
-						 : state === "exiting" ? "ModalClosed" : null
-					];
-
-					return (
-						<div className={cssClasses.join(" ")}>
-							<h1>A Modal</h1>
-							<button className="Button" onClick={props.close}>
-								Dismiss
-							</button>
-						</div>
-					)
-				}}
-
-			</Transition>
-		)
-
+const modal = (props) => {
+	return (
+		<CSSTransition 
+			in={props.show} 
+			timeout={300} 
+			mountOnEnter 
+			unmountOnExit
+			// Following classNames will be added to "Modal" className conditionally
+			classNames={{
+				enterActive: 'ModalOpen',
+				exitActive: 'ModalClosed'
+			}}
+		>
+			<div className={"Modal"}>
+				<h1>A Modal</h1>
+				<button className="Button" onClick={props.close}>
+					Dismiss
+				</button>
+			</div>
+		</CSSTransition>
+	)
 }
 
-export default modal;
+export default modal
